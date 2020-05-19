15eab14003f7913c8e8715b4953fa79a99c896b9
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-05-19 10:09:46 +0900

    클래스 추가

---
+++ StudentList/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentList
+{
+    class Student
+    {
+        public string name;
+        public string password;
+        public string address;
+        public string phoneNumber;
+        public DateTime regDate;
+    }
+}
